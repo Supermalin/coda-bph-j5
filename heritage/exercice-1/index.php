35e0b0aa08ab4347a92c655af2b2c40b4122d9d3
cizeautom <cizeautom404@gmail.com>
2025-11-27 14:22:59 +0100

    exercice 1 sur l'héritage des classe en php

--- heritage/exercice-1/index.php
+++ heritage/exercice-1/index.php
@@ -1,16 +1,58 @@
 <?php 
-class User
-{
-    protected string $email;
-    protected string $password;
+    class User
+    {
+        protected string $email;
+        protected string $password;
 
-    public function __construct(){
+        public function __construct(){
 
+        }
+
+        public function login() : array {
+            return ["login" => $this->email, "password" => $this->password];
+        }
     }
 
-    public function login() : array {
-    	return ["login" => $this->email, "password" => $this->password];
-    }
-}
+    class Reader extends User
+    {
+        private array $favoriteBooks = [];
 
+        public function __construct(string $email, string $password){
+            $this->email = $email;
+            $this->password = $password;
+        }
+
+        public function addBookToFavorites(string $book): array {
+            $this->favoriteBooks[] = $book;
+
+            return $this->favoriteBooks;
+        }
+
+        public function removeBookFromFavorites(string $book): array {
+            foreach($this->favoriteBooks as $key => $favoriteBook)
+            {
+                if($favoriteBook === $book)
+                {
+                    unset($this->favoriteBooks[$key]);
+                }
+            }
+
+            return $this->favoriteBooks;
+        }
+    }
+
+    $lecteur = new Reader("exemple@exemple.com", "motDePasse");
+    $books = $lecteur->addBookToFavorites("livre0");
+    $books = $lecteur->addBookToFavorites("livre1");
+    foreach ($books as $book){
+        echo $book."<br>";
+    }
+
+    $books = $lecteur->removeBookFromFavorites("livre1");
+
+    foreach ($books as $book){
+        echo $book."<br>";
+    }
+
+    echo $lecteur->login()['login']."<br>".$lecteur->login()['password'];
 ?>
