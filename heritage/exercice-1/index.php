f8d044367e56f11a679208feda1172a69bb031e3
cizeautom <cizeautom404@gmail.com>
2025-11-27 14:03:34 +0100

    mise en place de dossier les exercice sont triés

---
+++ heritage/exercice-1/index.php
@@ -0,0 +1,16 @@
+<?php 
+class User
+{
+    protected string $email;
+    protected string $password;
+
+    public function __construct(){
+
+    }
+
+    public function login() : array {
+    	return ["login" => $this->email, "password" => $this->password];
+    }
+}
+
+?>
